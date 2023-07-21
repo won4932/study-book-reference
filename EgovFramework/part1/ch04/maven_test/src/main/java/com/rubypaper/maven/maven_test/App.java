b08d1d68a0542fd0e96cfb872d4731cdb306262b
WontaeSeong <won4932@gmail.com>
2023-07-21 22:57:07 +0900

    study book refer merge -egov  - ch4 maven_test

---
+++ EgovFramework/part1/ch04/maven_test/src/main/java/com/rubypaper/maven/maven_test/App.java
@@ -0,0 +1,13 @@
+package com.rubypaper.maven.maven_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
