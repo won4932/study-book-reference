189e93cb62583d50629681794d2b43b16bfa9805
WontaeSeong <won4932@gmail.com>
2023-08-08 22:57:07 +0900

    study book refer merge -egov  - part2 - ch02

---
+++ EgovFramework/part2/ch02/SampleWeb/src/main/java/egovframework/sample/service/impl/SampleDAOJDBC.java
@@ -0,0 +1,103 @@
+package egovframework.sample.service.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import egovframework.sample.service.SampleDAO;
+import egovframework.sample.service.SampleVO;
+import egovframework.sample.service.common.JDBCUtil;
+
+@Repository("daoJDBC")
+public class SampleDAOJDBC implements SampleDAO {
+	// JDBC 관련 변수 
+	private Connection conn;
+	private PreparedStatement stmt;
+	private ResultSet rs;
+	
+	// SQL 명령어들
+	private final String SAMPLE_INSERT = "INSERT INTO SAMPLE(ID, TITLE, REG_USER, CONTENT, REG_DATE) VALUES ((SELECT NVL(MAX(ID), 0) + 1 FROM SAMPLE), ?, ?, ?, SYSDATE)";
+	private final String SAMPLE_UPDATE = "UPDATE SAMPLE SET TITLE=?, REG_USER=?, CONTENT=? WHERE ID=?";
+	private final String SAMPLE_DELETE = "DELETE FROM SAMPLE WHERE ID=?";
+	private final String SAMPLE_GET    = "SELECT ID, TITLE, REG_USER, CONTENT, REG_DATE FROM SAMPLE WHERE ID=?";
+	private final String SAMPLE_LIST = "SELECT ID, TITLE, REG_USER, CONTENT, REG_DATE FROM SAMPLE ORDER BY REG_DATE DESC";
+
+	public SampleDAOJDBC() {
+		System.out.println("===> SampleDAOJDBC 생성");
+	}
+	
+	public void insertSample(SampleVO vo) throws Exception {
+		System.out.println("===> JDBC로 insertSample() 기능 처리");
+		conn = JDBCUtil.getConnection();
+		stmt = conn.prepareStatement(SAMPLE_INSERT);
+		stmt.setString(1, vo.getTitle());
+		stmt.setString(2, vo.getRegUser());
+		stmt.setString(3, vo.getContent());
+		stmt.executeUpdate();
+		JDBCUtil.close(stmt, conn);
+	}
+	
+	public void updateSample(SampleVO vo) throws Exception {
+		System.out.println("===> JDBC로 updateSample() 기능 처리");
+		conn = JDBCUtil.getConnection();
+		stmt = conn.prepareStatement(SAMPLE_UPDATE);
+		stmt.setString(1, vo.getTitle());
+		stmt.setString(2, vo.getRegUser());
+		stmt.setString(3, vo.getContent());
+		stmt.setString(4, vo.getId());
+		stmt.executeUpdate();
+		JDBCUtil.close(stmt, conn);
+	}
+	
+	public void deleteSample(SampleVO vo) throws Exception {
+		System.out.println("===> JDBC로 deleteSample() 기능 처리");
+		conn = JDBCUtil.getConnection();
+		stmt = conn.prepareStatement(SAMPLE_DELETE);
+		stmt.setString(1, vo.getId());
+		stmt.executeUpdate();
+		JDBCUtil.close(stmt, conn);
+	}
+	
+	public SampleVO selectSample(SampleVO vo) throws Exception {
+		System.out.println("===> JDBC로 selectSample() 기능 처리");
+		SampleVO sample = null;
+		conn = JDBCUtil.getConnection();
+		stmt = conn.prepareStatement(SAMPLE_GET);
+		stmt.setString(1, vo.getId());
+		rs = stmt.executeQuery();
+		if(rs.next()) {
+			sample = new SampleVO();
+			sample.setId(rs.getString("ID"));
+			sample.setTitle(rs.getString("TITLE"));
+			sample.setRegUser(rs.getString("REG_USER"));
+			sample.setContent(rs.getString("CONTENT"));
+			sample.setRegDate(rs.getDate("REG_DATE"));
+		}			
+		JDBCUtil.close(rs, stmt, conn);
+		return sample;
+	}
+	
+	public List<SampleVO> selectSampleList(SampleVO vo) 
+		throws Exception {
+		System.out.println("===> JDBC로 selectSampleList() 처리");
+		List<SampleVO> sampleList = new ArrayList<SampleVO>();
+		conn = JDBCUtil.getConnection();
+		stmt = conn.prepareStatement(SAMPLE_LIST);
+		rs = stmt.executeQuery();
+		while(rs.next()) {
+			SampleVO 			sample = new SampleVO();
+			sample.setId(rs.getString("ID"));
+			sample.setTitle(rs.getString("TITLE"));
+			sample.setRegUser(rs.getString("REG_USER"));
+			sample.setContent(rs.getString("CONTENT"));
+			sample.setRegDate(rs.getDate("REG_DATE"));
+			sampleList.add(sample);
+		}			
+		JDBCUtil.close(rs, stmt, conn);
+		return sampleList;
+	}
+}
---
+++ EgovFramework/part2/ch02/SampleWeb/src/main/java/egovframework/sample/service/impl/SampleServiceImpl.java
@@ -0,0 +1,55 @@
+package egovframework.sample.service.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import egovframework.rte.fdl.idgnr.EgovIdGnrService;
+import egovframework.sample.service.SampleDAO;
+import egovframework.sample.service.SampleService;
+import egovframework.sample.service.SampleVO;
+
+@Service("sampleService")
+public class SampleServiceImpl implements SampleService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SampleServiceImpl.class);
+	
+	@Resource(name="daoSpring")
+	private SampleDAO sampleDAO;
+	
+	@Resource(name = "egovIdGnrService")
+	private EgovIdGnrService egovIdGnrService;
+
+	public void insertSample(SampleVO vo) throws Exception {	
+		LOGGER.trace("TRACE Level Logging");
+		LOGGER.debug("DEBUG Level Logging");
+		LOGGER.info("INFO Level Logging");
+		LOGGER.warn("WARN Level Logging");
+		LOGGER.error("ERROR Level Logging");
+		
+		/** ID Generation Service */
+		String id = egovIdGnrService.getNextStringId();
+		vo.setId(id);
+		sampleDAO.insertSample(vo);
+	}
+
+	public void updateSample(SampleVO vo) throws Exception {
+		sampleDAO.updateSample(vo);
+	}
+
+	public void deleteSample(SampleVO vo) throws Exception {
+		sampleDAO.deleteSample(vo);
+	}
+
+	public SampleVO selectSample(SampleVO vo) throws Exception {
+		return sampleDAO.selectSample(vo);
+	}
+
+	public List<SampleVO> selectSampleList(SampleVO vo) throws Exception {
+		return sampleDAO.selectSampleList(vo);
+	}
+}
