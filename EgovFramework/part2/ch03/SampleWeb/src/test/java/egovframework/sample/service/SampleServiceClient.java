f713f325dbeaf66544b3f17d9ba566ee8e2f75aa
WontaeSeong <won4932@gmail.com>
2023-09-06 22:57:07 +0900

    study book refer merge -egov  - part2 - ch03

---
+++ EgovFramework/part2/ch03/SampleWeb/src/test/java/egovframework/sample/service/SampleServiceClient.java
@@ -0,0 +1,30 @@
+package egovframework.sample.service;
+
+import java.util.List;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class SampleServiceClient {
+	public static void main(String[] args) throws Exception {
+		// 1. Spring 컨테이너를 구동한다.
+		AbstractApplicationContext container = new GenericXmlApplicationContext("egovframework/spring/context-*.xml");
+
+		// 2. Spring 컨테이너로부터 SampleServiceImpl 객체를 Lookup한다.
+		SampleService sampleService = (SampleService) container.getBean("sampleService");
+		SampleVO vo = new SampleVO();
+		vo.setTitle("아이디 자동 생성 테스트");
+		vo.setRegUser("테스터");
+		vo.setContent("아이디 자동 .....");
+//		sampleService.insertSample(vo);
+
+		List<SampleVO> sampleList = sampleService.selectSampleList(vo);
+		System.out.println("[ Sample LIST ]");
+		for (SampleVO sample : sampleList) {
+			System.out.println("---> " + sample.toString());
+		}
+
+		// 3. Spring 컨테이너를 종료한다.
+		container.close();
+	}
+}
